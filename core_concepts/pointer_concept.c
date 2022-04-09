08406bde08456261bfcdae2355b8110e4370d84d
Lawkay1 <lawkay4jesus@gmail.com>
2022-04-09 14:43:46 +0100

    Incrementing and decrementing pointers update

--- core_concepts/pointer_concept.c
+++ core_concepts/pointer_concept.c
@@ -6,14 +6,17 @@ int main () {
 int a = 20 ;
 int *p = &a ;
  printf ("%d \n", a);
- printf ("%p \n", p);
- printf ("%p \n", &p); 
+ printf ("%d \n", p);
+ printf ("%d \n", &p); 
  printf ("%d \n", *p);
-
- *p = 15 ;
+int b = 5;
+ *p = b ;
 
  printf("%d \n", a);
-
+printf ("%d", &a);
+/** Incrementing and Decrementing a pointer
+ * **/
+ printf ("Seeing the address of a \n as stated above, and knowing that the size of an integer is %d \n we can conclude that the adress a+1 is %d \n", sizeof(int), p+1);
  return 0; 
 
 }
