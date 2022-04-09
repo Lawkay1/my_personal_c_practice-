61ff5306e6e8f21bae571737d7237f0930555f48
Lawkay1 <lawkay4jesus@gmail.com>
2022-04-09 17:10:59 +0100

    pointer to pointer

--- core_concepts/pointer_concept.c
+++ core_concepts/pointer_concept.c
@@ -16,7 +16,15 @@ int b = 5;
 printf ("%d", &a);
 /** Incrementing and Decrementing a pointer
  * **/
- printf ("Seeing the address of a \n as stated above, and knowing that the size of an integer is %d \n we can conclude that the adress a+1 is %d \n", sizeof(int), p+1);
- return 0; 
+ printf ("Seeing the address of a \n as stated above, and knowing that the size of an integer is %d \n we can conclude that the adress a+1 is %d \n value at address P+1 is %d ", sizeof(int), p+1, *(p+1));
+ 
 
+ /** Pointer to pointer */
+int **y = &p; 
+int ***u = &y; 
+printf ("%d \n", y); 
+printf ("%d \n", u);
+printf ("%d \n", **y);
+printf ("%d \n", ***u);
+ return 0;
 }
